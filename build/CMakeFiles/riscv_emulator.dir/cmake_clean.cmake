dfeb10f7048cc5403d6f42f2c8f3b071e23c3063
Harsh Kanoje <129186823+harshkanoje@users.noreply.github.com>
2026-03-18 11:07:23 +0530

    Add files via upload

---
+++ build/CMakeFiles/riscv_emulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/riscv_emulator.dir/main.c.o"
+  "CMakeFiles/riscv_emulator.dir/main.c.o.d"
+  "CMakeFiles/riscv_emulator.dir/src/bus.c.o"
+  "CMakeFiles/riscv_emulator.dir/src/bus.c.o.d"
+  "CMakeFiles/riscv_emulator.dir/src/cpu.c.o"
+  "CMakeFiles/riscv_emulator.dir/src/cpu.c.o.d"
+  "CMakeFiles/riscv_emulator.dir/src/dram.c.o"
+  "CMakeFiles/riscv_emulator.dir/src/dram.c.o.d"
+  "riscv_emulator"
+  "riscv_emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/riscv_emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/riscv_emulator.dir/src/bus.c.o.d
@@ -0,0 +1,20 @@
+CMakeFiles/riscv_emulator.dir/src/bus.c.o: \
+ /home/harsh/RISC-V-Emu-Code-/src/bus.c /usr/include/stdc-predef.h \
+ /home/harsh/RISC-V-Emu-Code-/include/bus.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /home/harsh/RISC-V-Emu-Code-/include/dram.h
